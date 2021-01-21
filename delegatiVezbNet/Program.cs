eace201fb546d2b71e6d10362046dd894b7d39a7
Marko Djokic <marko1319@its.edu.rs>
2021-01-21 01:06:57 +0100

    Add project files.

---
+++ delegatiVezbNet/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace delegatiVezbNet
+{
+    class Program
+    {
+        delegate void Print(string parametar);
+        static public DateTime time;
+
+        //=================================================================================
+        static void LogIn(string name)
+        {
+            time = DateTime.Now;
+            WriteLine(name + " is log in " + time + Environment.NewLine);
+        }
+        //----------------------------------------------------------------
+        static void LogOut(string name)
+        {
+            time = DateTime.Now;
+            WriteLine(name + " is log out " + time + Environment.NewLine);
+        }
+        //=================================================================================
+
+        static void Main(string[] args)
+        {
+            Print guest1, guest2, guest3, guest4;
+            string line = new String('-', 20);//ISPIS 20 CRTICA....
+
+            guest1 = new Print(LogIn);
+            guest2 = new Print(LogOut);
+            guest3 = guest1 + guest2;
+            guest4 = guest3 - guest1;
+
+            WriteLine(line + Environment.NewLine);//----------------
+
+            //=================================================================================
+            WriteLine("Marko is going to log in! " + Environment.NewLine);
+            guest1("Marko");
+            WriteLine(line);
+
+            WriteLine("Stefan is going to log out! " + Environment.NewLine);
+            guest2("Stefan");
+            WriteLine(line);
+
+            WriteLine("Vukasin is going to log in  and log out! " + Environment.NewLine);
+            guest3("Vukasin");
+            WriteLine(line);
+
+            WriteLine("David is going to log in  and log out!  But we need only time when loged out!" + Environment.NewLine);
+            guest4("David");
+            WriteLine(line);
+            //=================================================================================
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ delegatiVezbNet2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace delegatiVezbNet2
+{
+    class Program
+    {
+        public delegate bool FilterDelegate(Employee employee);
+
+        public struct Employee // zaposleni 
+        {
+            public string Name;
+            public double Salary; //plata
+        }
+        //=========================================================================
+        //=========================================================================
+        static bool IsMinimum(Employee employee)
+        {
+            return employee.Salary < 1500;
+        }
+        static bool IsMedian(Employee employee)
+        {
+            return employee.Salary > 1500 & employee.Salary < 3000;
+        }
+        static bool IsMaximum(Employee employee)
+        {
+            return employee.Salary > 3000;
+        }
+        static void DisplaySalaries(string title, List<Employee> employees, FilterDelegate filter)
+        {
+            WriteLine(title + Environment.NewLine);
+
+            foreach(Employee employee in employees)
+            {
+                if (filter(employee))
+                {
+                    WriteLine($"{employee.Name}, {employee.Salary.ToString("C")}");//.ToString("C") STAMPA FUNTU
+                }
+            }
+            WriteLine(Environment.NewLine);
+        }
+        //=========================================================================
+        //=========================================================================
+        static void Main(string[] args)
+        {
+            string line = new String('-', 20);
+
+            Employee employee1;
+            employee1.Name = "Marko";
+            employee1.Salary = 4000;
+
+            Employee employee2;
+            employee2.Name = "Stefan";
+            employee2.Salary = 2000;
+
+            Employee employee3;
+            employee3.Name = "Vukasin";
+            employee3.Salary = 1200;
+
+            Employee employee4;
+            employee4.Name = "David";
+            employee4.Salary = 5000;
+
+            List<Employee> employees = new List<Employee> { employee1, employee2, employee3, employee4 };
+           
+            //=========================================================================
+            WriteLine("SALARIES: ");
+            WriteLine(line);
+
+            DisplaySalaries("Minimum salaries: ", employees, IsMinimum);
+            WriteLine(line);
+            DisplaySalaries("Median salaries: ", employees, IsMedian);
+            WriteLine(line);
+            DisplaySalaries("High salaries: ", employees, IsMaximum);
+            WriteLine(line);
+
+            ReadKey();
+        }
+    }
+}
